f3a9e82894d53ef860b52533146904dc04705924
Cinyu <DaisyGXY@outlook.com>
2023-10-18 10:58:44 +0800

    delete irrelevant docs

--- 004. Longest Palindromic Substring/TEST.cc
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "../Catch/single_include/catch.hpp"
-#include "solution.h"
-
-TEST_CASE("Longest Palindromic Substring", "longestPalindrome")
-{
-    Solution s;
-    REQUIRE( s.longestPalindrome("abbabbua") == "bbabb");
-}
--- 104. Construct Binary Tree from Preorder and Inorder Traversal/README.md
+++
@@ -1,3 +0,0 @@
-这道题与 [77. Construct Binary Tree from Inorder and Postorder Traversal](../77. Construct Binary Tree from Inorder and Postorder Traversal) 的思路几乎一模一样。
-
-所以不多做赘述了，解法上也没有太多创新，考点依旧是对二叉树深度遍历的理解。
